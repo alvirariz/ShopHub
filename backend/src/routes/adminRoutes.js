58e62cc9830c3567fef3477b69f7280ea5bf02ce
alvirariz <alvirarizwan@gmail.com>
2026-04-21 00:12:34 +0500

    Alvira Route to unban/ban feature added

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -4,8 +4,14 @@ const express = require('express')
 const router = express.Router()
 
 const { ViewSalesReport,viewCustomerInsights } = require('../controllers/adminController')
+const { manageUserStatus } = require('../controllers/adminController');
 
+// Simple route - anyone can access (FOR TESTING ONLY)
+router.patch('/users/:userId/status', manageUserStatus);
 router.get('/sales', ViewSalesReport)
 router.get('/insights', viewCustomerInsights)
+
+
+
 module.exports = router
 
