af9266171830cb3ba25b723f204a80e4234b2296
alvirariz <alvirarizwan@gmail.com>
2026-04-21 01:00:48 +0500

    Alvira- added get platform metrics route

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -4,9 +4,9 @@ const express = require('express')
 const router = express.Router()
 
 const { ViewSalesReport,viewCustomerInsights } = require('../controllers/adminController')
-const { manageUserStatus } = require('../controllers/adminController');
+const { manageUserStatus, getPlatformMetrics } = require('../controllers/adminController');
 
-// Simple route - anyone can access (FOR TESTING ONLY)
+router.get('/metrics', getPlatformMetrics);
 router.patch('/users/:userId/status', manageUserStatus);
 router.get('/sales', ViewSalesReport)
 router.get('/insights', viewCustomerInsights)
