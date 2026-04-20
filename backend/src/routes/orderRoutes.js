a7ab02612cbfa94ca63cc85d1418ec009cfc8a7a
rida288 <fatima166rida@gmail.com>
2026-04-20 11:27:19 +0500

    Rida - add checkout, order history, and track order routes

--- backend/src/routes/orderRoutes.js
+++ backend/src/routes/orderRoutes.js
@@ -4,10 +4,16 @@ const router = express.Router();
 
 const {
  viewIncomingOrders,
- viewSpecificOrders
+ viewSpecificOrders, 
+ checkout, 
+ viewOrderHistory,
+ trackOrderStatus
 } = require('../controllers/orderController');
 
 // Compare products
 router.get('/view', viewIncomingOrders);
+router.get('/history/:userId', viewOrderHistory);
+router.get('/track/:orderId', trackOrderStatus);
+router.post('/checkout', checkout);
 router.get('/:orderId', viewSpecificOrders );
 module.exports = router
